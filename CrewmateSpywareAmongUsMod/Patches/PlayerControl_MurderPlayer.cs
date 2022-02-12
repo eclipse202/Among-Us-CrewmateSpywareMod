bcbce725ac4df8b68a5c4f6a78005e8709ef8a96
Gaven <gavennowak20@gmail.com>
2022-02-12 16:19:57 -0500

    Add project files.

---
+++ CrewmateSpywareAmongUsMod/Patches/PlayerControl_MurderPlayer.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace CrewmateSpywareMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.MurderPlayer))]
+    internal class PlayerControl_MurderPlayer
+    {
+        [HarmonyPrefix]
+        internal static bool Prefix(PlayerControl __instance, PlayerControl target)
+        {
+            Main.Logger.LogMessage(__instance.Data.PlayerName + " killed " + target.name);
+            HudManager.Instance.ShowPopUp(__instance.Data.PlayerName + " killed " + target.name);
+            return true;
+        }
+    }
+}
